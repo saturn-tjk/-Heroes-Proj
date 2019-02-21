465f1cf714d369a52ddd4bcd156e00352303b923
Alisher Nuraliev <a.nuraliev85@gmail.com>
2019-02-21 23:25:04 +0300

    +

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,28 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import { BrowserModule }    from '@angular/platform-browser';
+import { NgModule }         from '@angular/core';
+import { FormsModule }      from '@angular/forms';
+import { HttpModule }       from '@angular/http';
 
-import { AppComponent } from './app.component';
+
+import { AppComponent }         from './app.component/app.component';
+import  { HeroDetailComponent } from  './app.component/hero-detail.component';
+import  { HeroService }         from './hero.service';
+import { AppComponent2 }        from './app.component2/app-component2';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroDetailComponent,
+    AppComponent2
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [HeroService],
+  bootstrap: [AppComponent, AppComponent2]
 })
 export class AppModule { }
